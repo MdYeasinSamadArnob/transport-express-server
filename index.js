c269cc95d709df2b3312489666a7cad9bd4d44cd
MdYeasinSamadArnob <yeasinsamadarnob@gmail.com>
2022-06-15 09:31:22 +0600

    3 files modified

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 // run `node index.js` in the terminal
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: '',
+  database: 'mysqlcrud',
+});
+
+//Root Url
+app.get('/', (req, res) => {
+  res.send('hello world, Ripon');
+});
